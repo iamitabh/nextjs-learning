7a5fc12fc7062fbfd676f65c14fc8c8d11be6188
iamitabh <amitabh.mishra.2002@gmail.com>
2024-05-25 16:21:32 +0530

    add lesson-01

--- Codevolution/lesson-01/index.js
+++ Codevolution/lesson-01/index.js
@@ -1,1 +1,23 @@
-// Lesson 01:
+// Lesson 01
+
+// What is Next.js?
+
+// It's not feasiable to create a fully-featured application ready for production. 
+// React is a library for building user interfaces.
+// You need to make decisions about other features such as routing, data fetching, and more.
+
+// Where as Next.js is a React framework.
+// It uses React for buidling user interfaces.
+// Provides additional features that enable you to build production-ready application.
+// These feature includes routing, optimized rendering, data fetching, bundiling, compiling, and more.
+// You don't need to install additional packages as Next.js provides everything you need.
+// Opinions and convetions should be followed to implement these features.
+
+// Why learn Next.js?
+
+// 1. Routing
+// 2. API routes
+// 3. Rendering
+// 4. Data fetching
+// 5. Styling
+// 6. Optimization
