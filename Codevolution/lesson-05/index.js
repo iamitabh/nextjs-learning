1c091d8eeea3d6f6aac44d792bb39f7f18f0661a
iamitabh <amitabh.mishra.2002@gmail.com>
2024-05-25 17:19:05 +0530

    add lesson-05 files

---
+++ Codevolution/lesson-05/index.js
@@ -0,0 +1,12 @@
+// Lesson 05
+
+// Next.js has a file system based routing mechanism
+
+// URL paths that users can access in the browser are defined by files and folders in your codebase.
+// There some rules that needs to be followed in order to get file-based routing -
+
+// 1. All routes must be placed inside the app folder
+// 2. Every file that corresponds to a route must be named page.js or page.tsx.
+// 3. Every folder corresponds to a path segment in the browser URL.
+
+// If you try to access a route that don't exist, Next.js automatically puts a 404 error page.
